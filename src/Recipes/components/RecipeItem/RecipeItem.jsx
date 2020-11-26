83bd8bf95608e9dcc679d420ebca750f5b2573bb
HP <HP@DESKTOP-8EO1B9C>
2020-11-26 13:13:30 +0300

    feat: created RecipeItem component

---
+++ src/Recipes/components/RecipeItem/RecipeItem.jsx
@@ -0,0 +1,48 @@
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+
+export function RecipeItem({
+  title, image, healthLabels, onClick, id, getId,
+}) {
+  const onClickHandler = useCallback(() => {
+    getId(id);
+    onClick();
+  }, [onClick, getId, id]);
+
+  return (
+    <StyledContainer id={id} getId={getId}>
+      <StyledImage src={`${image}`} onClick={onClickHandler} />
+      <StyledContentContainer>
+        <StyledTitle>{title}</StyledTitle>
+        <ol>
+          {healthLabels.map((text) => <li key={text}>{text}</li>)}
+        </ol>
+      </StyledContentContainer>
+    </StyledContainer>
+  );
+}
+
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  padding: 10px 15px;
+  width: 400px;
+  border: 1px solid black;
+`;
+
+const StyledImage = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  cursor: pointer;
+`;
+
+const StyledContentContainer = styled.div`
+  padding-left: 15px;
+`;
+
+const StyledTitle = styled.h2`
+  margin: 0;
+  margin-bottom: 25px;
+`;
